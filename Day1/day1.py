e9e930b24338ecf74b2c9a055a1b20645b088191
Thang Do <thang.m.do@sony.com>
2020-07-08 16:04:49 +0930

    Day 1: Part 2

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,9 +1,14 @@
-def calculate_fuel(fuel):
-    return (fuel // 3) - 2
+def calculate_fuel(mass):
+    fuel = (mass // 3) - 2
+    if fuel <= 0:
+        return 0
+
+    fuel += calculate_fuel(fuel)
+    return fuel
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        fuels = f.readlines()
+        weights = f.readlines()
 
-    total = sum([calculate_fuel(int(x)) for x in fuels])
+    total = sum([calculate_fuel(int(mass)) for mass in weights])
     print(total)
