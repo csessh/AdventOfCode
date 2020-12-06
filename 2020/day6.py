c43b5e9bd2f816cbff32ed01d2845c0bdb42a26b
Thang Do <thang.m.do@sony.com>
2020-12-06 21:44:11 +1030

    One liner is lame

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,6 +1,24 @@
 #!/Users/thangdo/Documents/dev/csessh/bin/python
 
-# People seem to think one-liner is cool.
+# People seem to think one liner is cool.
 # Here's one for you:
 print(sum([len(set(form.replace('\n', ''))) for form in open('test.txt', 'r').read().split('\n\n')]))
+
+# Personally, I think one liner is lame.
+with open('test.txt', 'r') as f:
+    groups = f.read().split('\n\n')
+
+    count = 0
+    for group in groups:
+        choices = []
+        people = group.split('\n')
+
+        for choice in people:
+            choices.append(set(choice))
+
+        common_questions = set.intersection(*choices)
+        if common_questions:
+            count += len(common_questions)
+
+    print(count)
 
