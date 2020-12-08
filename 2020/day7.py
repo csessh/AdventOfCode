e41dd942afeb72d12e2779c3236e45a171be72ba
Thang Do <thang.m.do@sony.com>
2020-12-08 11:57:23 +1030

    Shiny gold bag should be the new industry standard for holding sooo many bags

---
+++ 2020/day7.py
@@ -0,0 +1,74 @@
+#!/Users/thangdo/Documents/dev/csessh/bin/python
+import re
+
+
+with open('test.txt', 'r') as f:
+    ridiculous_rules = f.readlines()
+
+
+rules = {}
+
+
+def can_this_bag_hold_shiny_stuff(bag: str) -> bool:
+    if rules[bag] is None:
+        return False
+
+    if 'shiny gold' in rules[bag].keys():
+        return True
+    else:
+        for sub in rules[bag].keys():
+            if can_this_bag_hold_shiny_stuff(sub):
+                return True
+        return False
+
+
+def how_many_bags_can_one_bag_carries(rule: str):
+    global rules
+    bag, inside = rule.split('bags contain')
+
+    if 'no other bags' in inside:
+        rules[bag.strip()] = None
+    else:
+        content = {}
+        for sub_bag in inside.split(','):
+            quantity = re.match(r'^([0-9]+) (\w+ \w+).+$', sub_bag.strip()).group(1)
+            bag_type = re.match(r'^([0-9]+) (\w+ \w+).+$', sub_bag.strip()).group(2)
+
+            if bag_type.strip() in content:
+                raise ValueError
+
+            content[bag_type.strip()] = int(quantity)
+        rules[bag.strip()] = content
+
+
+def how_many_bags_can_fit_within_this_bag(root: dict, multiplier: int) -> int:
+    if root == None:
+        return 0
+
+    total = sum(root.values()) * multiplier
+    for bag, count in root.items():
+        total += how_many_bags_can_fit_within_this_bag(rules[bag], count*multiplier)
+    return total
+
+def solve_part1():
+    for rule in ridiculous_rules:
+        how_many_bags_can_one_bag_carries(rule)
+
+    count = 0
+    for bag in rules.keys():
+        if can_this_bag_hold_shiny_stuff(bag):
+            count += 1
+    print(count)
+
+
+def solve_part2():
+    for rule in ridiculous_rules:
+        how_many_bags_can_one_bag_carries(rule)
+
+    root = rules['shiny gold']
+    total = how_many_bags_can_fit_within_this_bag(root, 1)
+    print(total)
+
+
+solve_part1()
+solve_part2()
