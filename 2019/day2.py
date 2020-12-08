cbdef6a40a71f5830478e4f2bb309549cbda18f4
Thang Do <thang.m.do@sony.com>
2020-12-08 13:04:19 +1030

    nested for loops .. ftw?

--- 2019/day2.py
+++ 2019/day2.py
@@ -1,65 +1,44 @@
-POS_X_OFFSET = 1
-POS_Y_OFFSET = 2
-POS_Z_OFFSET = 3
-DEFAULT_COMMAND_OFFSET = 4
-
-ADDITION = 1
-MULTIPLY = 2
-HALT = 99
-
-TARGET = 19690720
-NOUN = 1
-VERB = 2
+#!/Users/thangdo/Documents/dev/csessh/bin/python
+from copy import deepcopy
 
 
-def get_result(noun, verb):
-    return (100 * noun) + verb
+with open('test.txt') as f:
+    instruction = [int(x) for x in f.readline().rstrip().split(',')]
 
 
-def init():
-    with open('input.txt') as f:
-        return [int(x) for x in f.readline().rstrip().split(',')]
-
-
-def execute(commands, noun, verb):
-    commands[NOUN] = noun
-    commands[VERB] = verb
+def produce_output(noun: int, verb: int) -> int:
+    wip = deepcopy(instruction)
+    wip[1] = noun
+    wip[2] = verb
 
     pointer = 0
-    while pointer < len(commands):
-        try:
-            operation = commands[pointer]
+    while wip[pointer] != 99:
+        opcode = wip[pointer]
+        op1 = wip[pointer+1]
+        op2 = wip[pointer+2]
+        loc = wip[pointer+3]
 
-            if operation == HALT:
-                operation += 1
-                return False
+        if opcode == 1:
+            wip[loc] = wip[op1] + wip[op2]
+        elif opcode == 2:
+            wip[loc] = wip[op1] * wip[op2]
 
-            x_index = commands[pointer+POS_X_OFFSET]
-            y_index = commands[pointer+POS_Y_OFFSET]
-            z_index = commands[pointer+POS_Z_OFFSET]
-
-            x = commands[x_index]
-            y = commands[y_index]
-            z = x + y if operation == ADDITION else x * y
-
-            commands[z_index] = z
-            pointer += DEFAULT_COMMAND_OFFSET
-
-            if z == TARGET:
-                return True
-        except IndexError:
-            return False
-
-    return False
+        pointer += 4
+        if pointer >= len(wip):
+            raise ValueError
+    return wip[0]
 
 
-if __name__ == '__main__':
+def retrace(target: int) -> int:
     for noun in range(0, 100):
         for verb in range(0, 100):
-            commands = init()
-
-            if execute(commands, noun, verb):
-                print(get_result(noun, verb))
-
+            result = produce_output(noun, verb)
+            if result == target:
+                return (100*noun) + verb
 
 
+# Part 1:
+print(produce_output(noun=12, verb=2))
+
+# Part 2:
+print(retrace(target=19690720))
