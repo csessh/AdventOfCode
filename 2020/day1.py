df5da1642226cab924f102c355939154a6dbf51e
Thang Do <thang.m.do@sony.com>
2020-12-09 21:27:56 +1030

    Day 9 feels like day 1: combination of numbers

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,39 +1,40 @@
 #!/Users/thangdo/Documents/dev/csessh/bin/python
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def read() -> set:
-    with open('test.txt') as f:
-        temp = f.readlines()
-        data = set(list(map(int, temp)))
-    return data
 
-def solve_part1(numbers: set, target: int=2020) -> Tuple[int, int]:
-    """
-    Part 1: Target is set to 2020, we look for 2 numbers that add up to target
-    """
+
+def find_combo_of_2(numbers: Union[set, list], target: int=2020) -> Tuple[int, int]:
     for number in numbers:
         the_other_half = target - number
         if the_other_half in numbers:
             return the_other_half, number
     return None, None
 
-def solve_part2(numbers: set, target: int=2020) -> Tuple[int, int, int]:
-    """
-    Part 2: Target is set to 2020, we look for 3 numbers that add up to target
-    """
+
+def find_combo_of_3(numbers: set, target: int=2020) -> Tuple[int, int, int]:
     for x in numbers:
         sub_target = target - x
-        y, z = solve_part1(numbers, sub_target)
+        y, z = find_combo_of_2(numbers, sub_target)
         if y and z:
             return(x, y, z)
     return None, None, None
 
 
 if __name__ == '__main__':
-    numbers = read()
-    a, b = solve_part1(numbers)
+    with open('test.txt') as f:
+        temp = f.readlines()
+        data = set(list(map(int, temp)))
+
+    """
+    Part 1: Target is set to 2020, we look for 2 numbers that add up to target
+    """
+    a, b = find_combo_of_2(data)
     print(a, b, a*b)
 
-    x, y, z = solve_part2(numbers)
+
+    """
+    Part 2: Target is set to 2020, we look for 3 numbers that add up to target
+    """
+    x, y, z = find_combo_of_3(data)
     print(x, y, z, x*y*z)
---
+++ 2020/day9.py
@@ -0,0 +1,43 @@
+#!/Users/thangdo/Documents/dev/csessh/bin/python
+
+from itertools import combinations
+from typing import Tuple
+from day1 import find_combo_of_2
+
+
+with open('test.txt', 'r') as f:
+    data = [int(x) for x in f.readlines()]
+
+
+PREAMBLE_OFFSET = 25
+
+
+# Part 1
+weakness = None
+for index, number in enumerate(data[PREAMBLE_OFFSET:]):
+    preambes = data[index:index+PREAMBLE_OFFSET]
+
+    a, b = find_combo_of_2(preambes, number)
+    if not a and not b:
+        weakness = number
+        break
+print(weakness)
+
+
+def find_contigous_set_of_numbers(target: int) -> Tuple[int, int]:
+    contigous_set = []
+    for number in data:
+        if sum(contigous_set) >= target:
+            for i in range(len(contigous_set)):
+                if sum(contigous_set[i:]) == target and len(contigous_set[i:]) > 1:
+                    return (min(contigous_set[i:]), max(contigous_set[i:]))
+            del contigous_set[0]
+            contigous_set.append(number)
+        elif sum(contigous_set) < target:
+            contigous_set.append(number)
+
+
+
+# Part 2
+a, b = find_contigous_set_of_numbers(weakness)
+print(a+b)
