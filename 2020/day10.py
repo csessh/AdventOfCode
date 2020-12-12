9f4149faddcbe920a3cde1d8cc14761f04d350a9
Thang Do <thang.m.do@sony.com>
2020-12-12 22:07:56 +1030

    Day 10: brain burner

---
+++ 2020/day10.py
@@ -0,0 +1,36 @@
+#!/Users/thangdo/Documents/dev/csessh/bin/python
+
+from itertools import tee
+from typing import Union
+
+
+def pairwise(iterable: Union[set, list]) -> list:
+    """
+    pairwise([0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22])
+        ->      [1, 3, 1, 1, 1, 3,  1,  1,  3,  1,  3,  3]
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+with open('test.txt', 'r') as f:
+    data = [int(x) for x in f.readlines()]
+    data += [0, max(data) + 3]
+    data = sorted(data)
+
+
+# Part 1
+jolts = [y-x for x,y in pairwise(data)]
+print(jolts.count(1) * jolts.count(3))
+
+
+# Part 2
+variations = {0: 1}
+for adapter in data[1:]:
+    three_jolt_configs = variations.get(adapter-3, 0)
+    two_jolt_config = variations.get(adapter-2, 0)
+    one_jolt_config = variations.get(adapter-1, 0)
+    variations[adapter] = three_jolt_configs + two_jolt_config + one_jolt_config
+
+print(variations[data[-1]])
