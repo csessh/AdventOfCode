a19cb4f14e19bc748c1aafbebf7ebae8bcac69e4
Thang Do <thang.m.do@sony.com>
2021-10-15 11:04:42 +1030

    Day 15 Part 2: More Memory and clean up

--- 2020/day15.py
+++ 2020/day15.py
@@ -2,56 +2,50 @@
 
 from typing import List
 
+PART_1_MAX_ITERATION = 2020
+PART_2_MAX_ITERATION = 30000000
 
-MAX_ITERATION = 30000000
-STARTING_NUMBER_TURN = -1
 
-
-def part1(data: List[int]) -> int:
+def last_spoken_number(data: List[int]):
     spoken = {}
     turn = 1
     last_spoken = None
 
     while True:
-        if turn > MAX_ITERATION:
+        if turn > PART_2_MAX_ITERATION:
             break
 
         if turn <= len(data):
             last_spoken = data[turn-1]
-            print(f'Turn {turn} - number spoken is a starting number: {last_spoken}')
+            # print(f'Turn {turn} - number spoken is a starting number: {last_spoken}')
             spoken[last_spoken] = [turn]
         else:
-            print(f'Turn {turn} - Considering {last_spoken}:', end=' ')
+            # print(f'Turn {turn} - Considering {last_spoken}:', end=' ')
             if last_spoken in spoken:
                 if len(spoken[last_spoken]) == 1:
                     last_spoken = 0
-                    print(f'It was the first time it had been spoken. Number spoken this turn: {last_spoken}')
+                    # print(f'It was the first time it had been spoken. Number spoken this turn: {last_spoken}')
                 else:
                     pen, last = spoken[last_spoken][-2], spoken[last_spoken][-1]
                     last_spoken = spoken[last_spoken][-1] - spoken[last_spoken][-2]
-                    print(f'It had been spoken previously in turn {pen} and {last}. Number spoken this turn: {last_spoken}')
+                    # print(f'It had been spoken previously in turn {pen} and {last}. Number spoken this turn: {last_spoken}')
             else:
                 last_spoken = 0
-                print(f'It had never been spoken previously. Number spoken this turn: {last_spoken}')
+                # print(f'It had never been spoken previously. Number spoken this turn: {last_spoken}')
 
             if last_spoken in spoken:
                 spoken[last_spoken].append(turn)
             else:
                 spoken[last_spoken] = [turn]
+
+        if turn in [PART_1_MAX_ITERATION, PART_2_MAX_ITERATION]:
+            print(f'Lask spoken number at {turn}th turn was {last_spoken}')
+
         turn += 1
 
-    return last_spoken
-
-# assert part1([1,3,2]) == 1
-# assert part1([2,1,3]) == 10
-# assert part1([1,2,3]) == 27
-# assert part1([2,3,1]) == 78
-# assert part1([3,2,1]) == 438
-# assert part1([3,1,2]) == 1836
 
 if __name__ == '__main__':
     with open('test.txt', 'r') as f:
         data = [int(num) for num in f.readline().split(',')]
 
-    # Part 1 and 2
-    print(f'Last spoken number at {MAX_ITERATION}th turn was {part1(data)}')
+    last_spoken_number(data)
