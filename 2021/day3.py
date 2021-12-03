146035629865dc4f1aa8d6d92b25457c4bb624dc
Thang Do <thang.m.do@sony.com>
2021-12-03 23:51:08 +1030

    Day 3: Refactored

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,5 +1,67 @@
 import numpy
 from collections import Counter
+from typing import List, Tuple
+
+
+def get_oxygen(data: List[List[int]]) -> int:
+    oxygen = ''
+    for index in range(len(data)):
+        counter = Counter(data[index]).most_common()
+        most_common = counter[0]
+
+        if len(counter) > 1:
+            least_common = counter[1]
+            if most_common[1] == least_common[1] and most_common[0] == 0:
+                most_common, least_common = least_common, most_common
+
+        oxygen += str(most_common[0])
+        valid_column_indices = numpy.where(data[index] != most_common[0])
+        data = numpy.delete(data, list(valid_column_indices), 1)
+
+    return int(oxygen, 2)
+
+
+def get_scrubber(data: List[List[int]]) -> int:
+    scrubber = ''
+    for index in range(len(data)):
+        counter = Counter(data[index]).most_common()
+        most_common = counter[0]
+
+        if len(counter) > 1:
+            least_common = counter[1]
+            if most_common[1] == least_common[1] and most_common[0] == 0:
+                most_common, least_common = least_common, most_common
+
+            least_common_bit = 0 if most_common[0] == 1 else 1
+        else:
+            least_common_bit = most_common[0]
+
+        scrubber += str(least_common_bit)
+        valid_column_indices = numpy.where(data[index] != least_common_bit)
+        data = numpy.delete(data, list(valid_column_indices), 1)
+
+    return int(scrubber, 2)
+
+
+def get_power_consumption(data: List[List[int]]) -> Tuple[int, int]:
+    gamma = ''
+    epsilon = ''
+    for bits in rotated_data:
+        counter = Counter(bits).most_common()
+        most_common = counter[0]
+        bit, _ = most_common
+        gamma += str(bit)
+
+        try:
+            least_common = counter[1]
+            bit, _ = least_common
+        except:
+            if bit == 0:
+                bit = 1
+        finally:
+            epsilon += str(bit)
+
+    return int(gamma, 2), int(epsilon, 2)
 
 
 if __name__ == '__main__':
@@ -27,9 +89,7 @@ if __name__ == '__main__':
                 [0 0 1 0 0 1 1 1 1 0 0 0]
         """
         data = numpy.array([
-            list(
-                map(int, list(line.strip()))
-            )
+            list(map(int, list(line.strip())))
             for line in f.readlines()
         ])
         rotated_data = numpy.rot90(data, k=1, axes=(1,0))
@@ -37,59 +97,12 @@ if __name__ == '__main__':
     #
     # Part 1
     #
-    gamma = ''
-    epsilon = ''
-    for bits in rotated_data:
-        counter = Counter(bits).most_common()
-        most_common = counter[0]
-        bit, _ = most_common
-        gamma += str(bit)
-
-        try:
-            least_common = counter[1]
-            bit, _ = least_common
-        except:
-            if bit == 0:
-                bit = 1
-        finally:
-            epsilon += str(bit)
-
-    print(int(gamma, 2) * int(epsilon, 2))
+    gamma, epsilon = get_power_consumption(rotated_data)
+    print(gamma * epsilon)
 
     #
     # Part 2
     #
-    oxygen = ''
-    for index in range(len(rotated_data)):
-        counter = Counter(rotated_data[index]).most_common()
-        most_common = counter[0]
-
-        if len(counter) > 1:
-            least_common = counter[1]
-            if most_common[1] == least_common[1] and most_common[0] == 0:
-                most_common, least_common = least_common, most_common
-
-        oxygen += str(most_common[0])
-        valid_column_indices = numpy.where(rotated_data[index] != most_common[0])
-        rotated_data = numpy.delete(rotated_data, list(valid_column_indices), 1)
-
-    scrubber = ''
-    rotated_data = numpy.rot90(data, k=1, axes=(1,0))
-    for index in range(len(rotated_data)):
-        counter = Counter(rotated_data[index]).most_common()
-        most_common = counter[0]
-
-        if len(counter) > 1:
-            least_common = counter[1]
-            if most_common[1] == least_common[1] and most_common[0] == 0:
-                most_common, least_common = least_common, most_common
-
-            least_common_bit = 0 if most_common[0] == 1 else 1
-        else:
-            least_common_bit = most_common[0]
-
-        scrubber += str(least_common_bit)
-        valid_column_indices = numpy.where(rotated_data[index] != least_common_bit)
-        rotated_data = numpy.delete(rotated_data, list(valid_column_indices), 1)
-
-    print(int(scrubber, 2) * int(oxygen, 2))
+    oxygen = get_oxygen(rotated_data)
+    scrubber = get_scrubber(rotated_data)
+    print(scrubber * oxygen)
