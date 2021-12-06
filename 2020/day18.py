4337554e060a3ef18ff795feaad77cfd22927973
Thang Do <thang.m.do@sony.com>
2021-12-06 11:06:43 +1030

    WIP: Day 19

--- 2020/day18.py
+++ 2020/day18.py
@@ -2,13 +2,13 @@ from typing import List
 
 
 def calculate(prob: str, order: List[str]):
-    if "(" in prob:
+    if '(' in prob:
         openp = 0
-        start = prob.index("(")
+        start = prob.index('(')
         for i in range(start, len(prob)):
-            if prob[i] == "(":
+            if prob[i] == '(':
                 openp += 1
-            elif prob[i] == ")":
+            elif prob[i] == ')':
                 openp -= 1
             if openp == 0:
                 break
@@ -18,19 +18,19 @@ def calculate(prob: str, order: List[str]):
             order
         )
     else:
-        grams = prob.split(" ")
+        grams = prob.split(' ')
         for ops in order:
             i = 1
             while i < len(grams):
                 if grams[i] in ops:
                     grams = (
                         grams[: i - 1]
-                        + [str(eval(" ".join(grams[i - 1 : i + 2])))]
+                        + [str(eval(' '.join(grams[i - 1 : i + 2])))]
                         + grams[i + 2 :]
                     )
-
                 else:
                     i += 2
+
         return grams[0]
 
 
@@ -38,8 +38,8 @@ if __name__ == '__main__':
     with open('test.txt') as f:
         problems = f.readlines()
 
-    part1 = sum([int(calculate(problem, ["+*"])) for problem in problems])
-    print(f"Part 1: {part1}")
+    part1 = sum([int(calculate(problem, ['+*'])) for problem in problems])
+    print(f'Part 1: {part1}')
 
-    part2 = sum([int(calculate(problem, ["+", "*"])) for problem in problems])
-    print(f"Part 2: {part2}")
+    part2 = sum([int(calculate(problem, ['+', '*'])) for problem in problems])
+    print(f'Part 2: {part2}')
---
+++ 2020/day19.py
@@ -0,0 +1,59 @@
+import re
+from typing import List, Set, Dict
+
+
+RULE_PATTERNS = r'([0-9]+): (?:([0-9]+ [0-9]+(?:\s*\|*\s*[0-9]+ [0-9]+)*)|(?:\"([a-z]+)\"))'
+rules = {}
+
+def construct_possible_matches(rules: Dict[int, List[List[int]]]) -> Set[str]:
+    root = rules[0]
+    if len(root) == 1:
+        pass
+    else:
+        pass
+
+    return {}
+
+
+if __name__ == '__main__':
+    with open('test.txt') as f:
+        rules = {}
+
+        # Parse rules
+        while True:
+            rule = f.readline().strip()
+            if rule == '':
+                break
+
+            """
+                regex match groups ->
+                    ('0', '4 1', None)
+                    ('1', '2 3 | 3 2', None)
+                    ('2', '4 4 | 5 5', None)
+                    ('3', '4 5 | 5 4', None)
+                    ('4', None, 'a')
+                    ('5', None, 'b')
+
+                converted to a dictionary -> {
+                    0: [[4, 1]],
+                    1: [[2, 3], [3, 2]],
+                    2: [[4, 4], [5, 5]],
+                    3: [[4, 5], [5, 4]],
+                    4: 'a',
+                    5: 'b'
+                }
+            """
+            match = re.match(RULE_PATTERNS, rule)
+            if match.groups()[1]:
+                rules[int(match.groups()[0])] = [
+                    list(map(int, x.strip().split(' '))) for x in
+                    match.groups()[1].split('|')
+                ]
+            else:
+                rules[int(match.groups()[0])] = match.groups()[2]
+
+        # Parse messages
+        messages = set([line.strip() for line in f.readlines()])
+        possible_matches = construct_possible_matches(rules)
+        matches = messages.intersection(possible_matches)
+        print(f'Part 1: {len(matches)}')
