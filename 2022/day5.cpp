bf4f06976439106a8aa3da6826a1c8493feecae3
Thang Do <thang.m.do@sony.com>
2022-12-06 02:38:05 +1030

    Day 5: Late night AoC with #JPNCRO game. Japan leading by 1

--- 2022/day5.cpp
+++ 2022/day5.cpp
@@ -1,25 +1,174 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <regex>
 #include "helper.h"
+
+typedef std::vector<std::string> Instructions;
+typedef std::vector<std::string> Crates;
 
 const std::string DAY = "input/day5";
 
 //--------------------------------------------------------------------------------
+class CargoCrane {
+public:
+    std::map<unsigned int, Crates*> stacks;
+    size_t size;
+
+    CargoCrane(const std::string &labels) {
+        std::smatch match;
+
+        // Labels are organised in ascending order from left to right.
+        // For example:  1   2   3 ... 100
+        // 100 would be the size of this Cargo ship
+        if (std::regex_search(labels, match, std::regex("([0-9]+)\\s*$")))
+            this->size = std::stoi(match[1]);
+
+        for (auto i = 1; i <= this->size; i++)
+            this->stacks[i] = new Crates();
+    }
+
+    ~CargoCrane() {
+        std::cout << "Sinking the cargo ship ... bye!" << std::endl;
+        for (auto i = 1; i <= this->size; i++)
+            delete this->stacks[i];
+    }
+
+    /// @brief Part 1 requires a read of all labels of the top creates on each stack
+    void read_top_labels() {
+        std::cout << "Top labels: ";
+        for (auto&& stack : stacks)
+            std::cout << stack.second->back()[1];
+        std::cout << std::endl;
+    }
+
+    /// @brief Display the current state of the Cargo ship on screen
+    /// @param pretty Crates are presented in horizontal by default. Vertical stacks are prettier
+    void draw(bool pretty=false) {
+        if (pretty) {
+            return;
+        }
+        else {
+            for (auto&& stack: this->stacks) {
+                std::cout << stack.first << ": ";
+                for (auto&& crate : *stack.second) {
+                    std::cout << crate << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    /// @brief Crates are loaded and sorted into their appropriate stacks
+    /// @param crates Crates come in batch i.e. [Z] [M] [P]
+    void load(const std::string &crates) {
+        auto index = 0;
+
+        for (auto i=1; i <= this->size; i++) {
+            if (index > crates.length())
+                break;
+
+            auto crate = crates.substr(index, 3);
+
+            if (crate != "   ")
+                this->stacks.at(i)->push_back(crate);
+
+            index += 4;
+        }
+    }
+
+    /// @brief Take a single line of command and execute.
+    /// @param command i.e. "move 26 from 1 to 7"
+    void execute(const std::string &command, bool reserve_order=false) {
+        std::smatch matches;
+
+        unsigned int quantity = 0;
+        unsigned int source = 0;
+        unsigned int destination = 0;
+
+        if (std::regex_search(command, matches, std::regex("move ([0-9]+) from ([0-9]+) to ([0-9]+)"))) {
+            quantity = std::stoi(matches[1].str());
+            source = std::stoi(matches[2].str());
+            destination = std::stoi(matches[3].str());
+        }
+
+        if (!reserve_order)
+            this->move(quantity, source, destination);
+        else
+            this->ordered_move(quantity, source, destination);
+    }
+private:
+    void ordered_move(unsigned int &quantity, unsigned int &source, unsigned int &destination) {
+        Crates temp = {};
+        for (auto i=0; i < quantity; i++) {
+            auto crate = this->stacks[source]->back();
+            this->stacks[source]->pop_back();
+            temp.push_back(crate);
+        }
+
+        std::reverse(temp.begin(), temp.end());
+
+        for (auto&& crate : temp)
+            this->stacks[destination]->push_back(crate);
+    }
+
+    void move(unsigned int &quantity, unsigned int &source, unsigned int &destination) {
+        for (auto i=0; i < quantity; i++) {
+            auto crate = this->stacks[source]->back();
+            this->stacks[source]->pop_back();
+            this->stacks[destination]->push_back(crate);
+        }
+    }
+};
+
+//--------------------------------------------------------------------------------
+void process(const Instructions &instructions, const Crates &data, const std::string &labels, bool reserve_order=false) {
+    auto pship = new CargoCrane(labels);
+
+    for (auto&& intake : data)
+        pship->load(intake);
+
+    for (auto&& command : instructions)
+        pship->execute(command, reserve_order);
+
+    pship->read_top_labels();
+    delete pship;
+}
+
+
+//--------------------------------------------------------------------------------
 int main() {
-    std::vector<std::string> data = {};
+    Crates data = {};
+    Instructions commands = {};
+    auto is_cargo_loaded = false;
 
     std::ifstream file(DAY);
     if (file.is_open()) {
         std::string line;
         while (std::getline(file, line)) {
-            data.push_back(line);
+            if (line.empty()) {
+                is_cargo_loaded = true;
+                continue;
+            }
+
+            if (!is_cargo_loaded)
+                data.push_back(line);
+            else
+                commands.push_back(line);
         }
         file.close();
     }
 
-    // std::cout << "Part 1: " << part1 << std::endl;
-    // std::cout << "Part 2: " << part2 << std::endl;
+    auto labels = data.back();
+    data.pop_back();
+
+    // reverse the order of input to simulate a queue of incoming crates for the ship to load.
+    std::reverse(data.begin(), data.end());
+
+    process(commands, data, labels, false);
+    process(commands, data, labels, true);
 
     return 0;
 }
