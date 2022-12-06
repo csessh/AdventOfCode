dbabd59c655bbb51d4be4ea9743656ed7a43fa50
Thang Do <thang.m.do@sony.com>
2022-12-06 18:16:06 +1030

    cleaning is my favourite thing to do, honestly

--- 2022/day6.cpp
+++ 2022/day6.cpp
@@ -5,12 +5,11 @@
 const std::string DAY = "input/day6";
 
 //--------------------------------------------------------------------------------
-const unsigned int scan_for_start_of_packet_marker(const std::string data, size_t size) {
+unsigned int get_marker_index(const std::string data, size_t size) {
     for (auto i = 0; i < data.length()-size; i++) {
         std::set<char> uniques;
-        uniques.insert(data[i]);
 
-        for (auto j = 1; j<size; j++) {
+        for (auto j = 0; j < size; j++) {
             if (uniques.find(data[i+j]) == uniques.end())
                 uniques.insert(data[i+j]);
         }
@@ -24,18 +23,15 @@ const unsigned int scan_for_start_of_packet_marker(const std::string data, size_
 
 //--------------------------------------------------------------------------------
 int main() {
-    auto marker_index = 0;
-
     std::ifstream file(DAY);
     if (file.is_open()) {
         std::string line;
         while (std::getline(file, line))
-            marker_index = scan_for_start_of_packet_marker(line, 14);
+            std::cout << "Marker is at index " << get_marker_index(line, 14) << std::endl;
 
         file.close();
     }
 
-    std::cout << "Marker is at index " << marker_index << std::endl;
 
     return 0;
 }
