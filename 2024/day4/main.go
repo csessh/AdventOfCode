d2df0648e197de4681f01da6127ca867f145ff64
Thang Do <csessh@hey.com>
2024-12-04 20:14:42 +1030

    Day 4 - Ceres Search

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -16,11 +16,143 @@ func main() {
 	fmt.Printf("Part 2 = %d\n", part2(data))
 }
 
+func isMASX(r int, c int, data []string) bool {
+	if data[r][c] != 'A' {
+		panic("Nah")
+	}
 
-func part1(data []string) int {
-	return 0
+	w := len(data[0])
+	h := len(data)
+
+	var tl byte
+	var br byte
+
+	// top left
+	if r-1 >= 0 && c-1 >= 0 {
+		tl = data[r-1][c-1]
+	}
+
+	// bottom right
+	if r+1 < h && c+1 < w {
+		br = data[r+1][c+1]
+	}
+
+	if !(tl != br && (tl == 'M' || tl == 'S') && (br == 'M' || br == 'S')) {
+		return false
+	}
+
+	var tr byte
+	var bl byte
+
+	// top right
+	if r-1 >= 0 && c+1 < w {
+		tr = data[r-1][c+1]
+	}
+
+	// bottom left
+	if r+1 < h && c-1 >= 0 {
+		bl = data[r+1][c-1]
+	}
+
+	if !(tr != bl && (tr == 'M' || tr == 'S') && (bl == 'M' || bl == 'S')) {
+		return false
+	}
+
+	return true
 }
 
-func part2(data []string) int { 
-	return 0
+func findXmas(r int, c int, data []string) int {
+	if data[r][c] != 'X' {
+		panic("It's not Xmas yet")
+	}
+
+	w := len(data[0])
+	h := len(data)
+	count := 0
+
+	// up
+	if r-3 >= 0 {
+		if data[r-1][c] == 'M' && data[r-2][c] == 'A' && data[r-3][c] == 'S' {
+			count += 1
+		}
+	}
+
+	// down
+	if r+3 < h {
+		if data[r+1][c] == 'M' && data[r+2][c] == 'A' && data[r+3][c] == 'S' {
+			count += 1
+		}
+	}
+
+	// right
+	if c+3 < w {
+		if data[r][c+1] == 'M' && data[r][c+2] == 'A' && data[r][c+3] == 'S' {
+			count += 1
+		}
+	}
+
+	// left
+	if c-3 >= 0 {
+		if data[r][c-1] == 'M' && data[r][c-2] == 'A' && data[r][c-3] == 'S' {
+			count += 1
+		}
+	}
+
+	// top left
+	if r-3 >= 0 && c-3 >= 0 {
+		if data[r-1][c-1] == 'M' && data[r-2][c-2] == 'A' && data[r-3][c-3] == 'S' {
+			count += 1
+		}
+	}
+
+	// top right
+	if r-3 >= 0 && c+3 < w {
+		if data[r-1][c+1] == 'M' && data[r-2][c+2] == 'A' && data[r-3][c+3] == 'S' {
+			count += 1
+		}
+	}
+
+	// bottom left
+	if r+3 < h && c-3 >= 0 {
+		if data[r+1][c-1] == 'M' && data[r+2][c-2] == 'A' && data[r+3][c-3] == 'S' {
+			count += 1
+		}
+	}
+
+	// bottom right
+	if r+3 < h && c+3 < w {
+		if data[r+1][c+1] == 'M' && data[r+2][c+2] == 'A' && data[r+3][c+3] == 'S' {
+			count += 1
+		}
+	}
+
+	return count
+}
+
+func part1(data []string) int {
+	count := 0
+
+	for i, row := range data {
+		for j, c := range row {
+			if c == 'X' {
+				count += findXmas(i, j, data)
+			}
+		}
+	}
+
+	return count
+}
+
+func part2(data []string) int {
+	count := 0
+
+	for i, row := range data {
+		for j, c := range row {
+			if c == 'A' && isMASX(i, j, data) {
+				count += 1
+			}
+		}
+	}
+
+	return count
 }
--- 2024/day4/main_test.go
+++ 2024/day4/main_test.go
@@ -5,21 +5,74 @@ import (
 )
 
 var sample = []string {
-	"test",
+	"MMMSXXMASM",
+	"MSAMXMSMSA",
+	"AMXSXMAAMM",
+	"MSAMASMSMX",
+	"XMASAMXAMM",
+	"XXAMMXXAMA",
+	"SMSMSASXSS",
+	"SAXAMASAAA",
+	"MAMMMXMMMM",
+	"MXMXAXMASX",
+}
+
+
+var trimmed_sample1 = []string {
+	"....XXMAS.",
+	".SAMXMS...",
+	"...S..A...",
+	"..A.A.MS.X",
+	"XMASAMX.MM",
+	"X.....XA.A",
+	"S.S.S.S.SS",
+	".A.A.A.A.A",
+	"..M.M.M.MM",
+	".X.X.XMASX",
+}
+
+
+var trimmed_sample2 = []string {
+	".M.S......",
+	"..A..MSMS.",
+	".M.S.MAA..",
+	"..A.ASMSM.",
+	".M.S.M....",
+	"..........",
+	"S.S.S.S.S.",
+	".A.A.A.A..",
+	"M.M.M.M.M.",
+	"..........",
 }
 
 func TestPart1(t *testing.T) {
-	want := 0
+	want := 18
 
 	if got := part1(sample); got != want {
 		t.Errorf("got = %v, want %v", got, want)
 	}
 }
 
+func TestPart1UsingTrimmedSample(t *testing.T) {
+	want := 18
+
+	if got := part1(trimmed_sample1); got != want {
+		t.Errorf("got = %v, want %v", got, want)
+	}
+}
+
 func TestPart2(t *testing.T) {
-	want := 0
+	want := 9
 
 	if got := part2(sample); got != want {
+		t.Errorf("got = %v, want %v", got, want)
+	}
+}
+
+func TestPart2UsingTrimmedSample(t *testing.T) {
+	want := 9
+
+	if got := part2(trimmed_sample2); got != want {
 		t.Errorf("got = %v, want %v", got, want)
 	}
 }
