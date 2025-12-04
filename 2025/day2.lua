05472c69b2dcd39785131d7d21b25418725854dc
Thang Do <csessh@hey.com>
2025-12-04 15:54:31 +0700

    spaces vs tabs. spaces.

--- 2025/day2.lua
+++ 2025/day2.lua
@@ -1,78 +1,78 @@
 local elf = require("utils.helper")
 
 local function is_invalid_two_part_pattern(n)
-	local t = tostring(n)
-	if #t % 2 ~= 0 then
-		return false
-	end
+    local t = tostring(n)
+    if #t % 2 ~= 0 then
+        return false
+    end
 
-	local mid = #t / 2
-	local left = string.sub(t, 1, mid)
-	local right = string.sub(t, mid + 1)
-	return left == right
+    local mid = #t / 2
+    local left = string.sub(t, 1, mid)
+    local right = string.sub(t, mid + 1)
+    return left == right
 end
 
 -- KMP algorithm to find substring periodicity
 local function kmp(s)
-	local fail = {}
-	for i = 1, #s do
-		fail[i] = 0
-	end
+    local fail = {}
+    for i = 1, #s do
+        fail[i] = 0
+    end
 
-	for i = 2, #s do
-		local j = fail[i - 1]
-		while j > 0 and s:byte(i) ~= s:byte(j + 1) do
-			j = fail[j]
-		end
+    for i = 2, #s do
+        local j = fail[i - 1]
+        while j > 0 and s:byte(i) ~= s:byte(j + 1) do
+            j = fail[j]
+        end
 
-		if s:byte(i) == s:byte(j + 1) then
-			j = j + 1
-		end
+        if s:byte(i) == s:byte(j + 1) then
+            j = j + 1
+        end
 
-		fail[i] = j
-	end
+        fail[i] = j
+    end
 
-	local period_len = #s - fail[#s]
-	if #s % period_len == 0 then
-		return s:sub(1, period_len)
-	end
+    local period_len = #s - fail[#s]
+    if #s % period_len == 0 then
+        return s:sub(1, period_len)
+    end
 
-	return s
+    return s
 end
 
 local function main()
-	local part1 = 0
-	local part2 = 0
-	local content
+    local part1 = 0
+    local part2 = 0
+    local content
 
-	local file = io.open("puzzle.txt", "r")
-	if file ~= nil then
-		content = file:read("*a")
-		file:close()
-	end
+    local file = io.open("puzzle.txt", "r")
+    if file ~= nil then
+        content = file:read("*a")
+        file:close()
+    end
 
-	local ranges = elf.split(content, ",")
-	for _, v in ipairs(ranges) do
-		local start_id
-		local end_id
+    local ranges = elf.split(content, ",")
+    for _, v in ipairs(ranges) do
+        local start_id
+        local end_id
 
-		local range = elf.split(v, "-")
-		start_id = tonumber(range[1])
-		end_id = tonumber(range[2])
+        local range = elf.split(v, "-")
+        start_id = tonumber(range[1])
+        end_id = tonumber(range[2])
 
-		for n = start_id, end_id do
-			if is_invalid_two_part_pattern(n) then
-				part1 = part1 + n
-			end
+        for n = start_id, end_id do
+            if is_invalid_two_part_pattern(n) then
+                part1 = part1 + n
+            end
 
-			if kmp(tostring(n)) ~= tostring(n) then
-				part2 = part2 + n
-			end
-		end
-	end
+            if kmp(tostring(n)) ~= tostring(n) then
+                part2 = part2 + n
+            end
+        end
+    end
 
-	print("Part 1: ", part1)
-	print("Part 2: ", part2)
+    print("Part 1: ", part1)
+    print("Part 2: ", part2)
 end
 
 assert(is_invalid_two_part_pattern("1") == false, "1 should be a valid id")
--- 2025/utils/helper.lua
+++ 2025/utils/helper.lua
@@ -1,43 +1,43 @@
 M = {}
 
 function M.load_puzzle_input()
-	local file = io.open("puzzle.txt", "r")
-	if file ~= nil then
-		local content = file:read("*a")
-		file:close()
-		return content
-	end
+    local file = io.open("puzzle.txt", "r")
+    if file ~= nil then
+        local content = file:read("*a")
+        file:close()
+        return content
+    end
 end
 
 function M.split(str, c)
-	if str == nil then
-		error("The string is nil, dummy")
-	end
+    if str == nil then
+        error("The string is nil, dummy")
+    end
 
-	if c == nil then
-		error("The separator character is nil, dummy")
-	end
+    if c == nil then
+        error("The separator character is nil, dummy")
+    end
 
-	local result = {}
-	for word in string.gmatch(str, "([^" .. c .. "]+)") do
-		table.insert(result, word)
-	end
-	return result
+    local result = {}
+    for word in string.gmatch(str, "([^" .. c .. "]+)") do
+        table.insert(result, word)
+    end
+    return result
 end
 
 function M.char_at(s, i)
-	return s:sub(i, i)
+    return s:sub(i, i)
 end
 
 function M.replace_at(s, i, c)
-	return s:sub(1, i - 1) .. c .. s:sub(i + 1)
+    return s:sub(1, i - 1) .. c .. s:sub(i + 1)
 end
 
 function M.display_grid(grid)
-	for r = 1, #grid do
-		local line = grid[r]
-		print(line)
-	end
+    for r = 1, #grid do
+        local line = grid[r]
+        print(line)
+    end
 end
 
 return M
